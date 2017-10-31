83445472d447600d13d7dedc8a66f4f41a3bf179
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-10-31 15:26:31 +0200

    added the root url and temporary media files

--- server/heltech/urls.py
+++ server/heltech/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -24,4 +25,5 @@ urlpatterns = [
     url(r'^contacts/', include('contacts.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='main.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
