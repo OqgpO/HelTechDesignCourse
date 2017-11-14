664efdcab48200d98cff3e2f021360a9ac23e0ae
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-14 20:10:05 +0200

    dont ever debug over repo, its not good practice

--- server/api/views.py
+++ server/api/views.py
@@ -3,9 +3,8 @@ from __future__ import unicode_literals
 from events.models import Event
 from contacts.models import Speaker, Organisation
 from api.serializers import *
-from rest_framework import viewsets
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 import datetime
 from django.utils import timezone
@@ -30,8 +29,8 @@ class PartnerViewSet(viewsets.ModelViewSet):
     queryset = Organisation.objects.filter(is_partner=True)
     serializer_class = OrganisationSerializer
 
-@csrf_exempt
 @api_view(['GET'])
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 def pastEvents(request, limit=None):
     if request.method=='GET':
         if limit == None:
@@ -51,15 +50,16 @@ def pastEvents(request, limit=None):
     return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 def futureEvents(request, limit=None):
     if request.method=='GET':
         if limit == None:
-            events = event.objects.filter(start_time__gte=timezone.now()).order_by('+start_time')
+            events = event.objects.filter(start_time__gte=timezone.now()).order_by('start_time')
         else:
             try:
-                events = event.objects.filter(start_time__gte=timezone.now()).order_by('+start_time')[:limit]
+                events = event.objects.filter(start_time__gte=timezone.now()).order_by('start_time')[:limit]
             except IndexError:
-                events = event.objects.filter(start_time__gte=timezone.now()).order_by('+start_time')
+                events = event.objects.filter(start_time__gte=timezone.now()).order_by('start_time')
             except:
                 return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -71,10 +71,11 @@ def futureEvents(request, limit=None):
 
 
 @api_view(['GET'])
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 def currentEvent(request):
     if request.method=='GET':
         now = timezone.now()
-        event = Event.objects.filter(start_time__gte=now).order_by('+start_time')
+        event = Event.objects.filter(start_time__gte=now).order_by('start_time')
         if not event:
             event = Event.objects.filter(start_time__lte=now).order_by('-start_time')
             if not event:
