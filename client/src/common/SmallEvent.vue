8f6168aa3214834b9acf8aa7fd5582d4634c15ca
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-22 18:17:25 +0200

    v-if

--- client/src/common/SmallEvent.vue
+++ client/src/common/SmallEvent.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="main-event-column">
 <div class="image-wrapper">
-<router-link v-bind:to="eventUri()"><img v-bind:src="event.cover_uri" v-bind:alt="event.title"></router-link>
+<router-link v-if="event" v-bind:to="eventUri()"><img v-bind:src="event.cover_uri" v-bind:alt="event.title"></router-link>
 </div>
 <div class="events-title">
-<h2><router-link v-bind:to="eventUri()">{{event.title || "Not Found"}}</router-link></h2>
+<h2><router-link v-bind:to="eventUri()">{{event.title}}</router-link></h2>
 </div>
-<p>{{event.description | truncate(210, '...') || "Not Found"}}</p>
+<p>{{event.description | truncate(210, '...')}}</p>
 </div>
 
 </template>
@@ -23,9 +23,9 @@
             eventUri: function() {
                 var ret = "event/"
                 if (this.event) {
-                    return ret
-                } else {
                     return ret + this.event.id
+                } else {
+                    return ret
                 }
             },
         }
