0d3d144003cee6dba7b7bd712ab6fec133204e91
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-22 17:24:44 +0200

    removed the method call from binding

--- client/src/events/Events.vue
+++ client/src/events/Events.vue
@@ -29,7 +29,7 @@ We want to achieve this by discovering a different emerging technology topic eac
 <h2>Upcoming Events</h2>
 <div class="up-events">
 
-<SmallEvent v-for="event in future" v-bind:key="event.id" class="events-column" v-bind:class="{getEventClass(event.id)}" v-bind:event="event"></SmallEvent>
+<SmallEvent v-for="event in future" v-bind:key="event.id" class="events-column event-1" v-bind:event="event"></SmallEvent>
     </div>
     </div>
 <div id="past-events"> <!--vue.js time!-->
