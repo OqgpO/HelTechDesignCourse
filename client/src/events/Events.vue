fb365ee2f354c9fcdf7c82dae5f806ae341bac09
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-22 17:45:37 +0200

    argument pass comparison

--- client/src/events/Events.vue
+++ client/src/events/Events.vue
@@ -26,7 +26,7 @@ We want to achieve this by discovering a different emerging technology topic eac
 
 
 <div id="upcomming-events"> <!--vue.js time!-->
-<h2>Upcoming FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEvents</h2>
+<h2>Upcoming Events</h2>
 <div class="up-events">
 
 <SmallEvent v-for="event in future" v-bind:key="event.id" class="events-column event-1" v-bind:event="event"></SmallEvent>
@@ -40,7 +40,7 @@ We want to achieve this by discovering a different emerging technology topic eac
 <li v-for="item in future" :key="item.id">{{item.name}}</li>
 </ul>
 
-<SmallEvent v-for="event in past" :key="event.id" v-bind:class="getEventClass({event.id})" v-bind:event="event"></SmallEvent>
+<SmallEvent v-for="event in past" :key="event.id" v-bind:class="getEventClass(event.id)" v-bind:event="event"></SmallEvent>
     </div>
     </div>
     </div>
