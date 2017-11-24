e0f4a2afc14615d446d92c7c36cb56a89aed1a3f
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-24 11:40:49 +0200

    styling

--- client/src/landing/BottomRegion.vue
+++ client/src/landing/BottomRegion.vue
@@ -38,12 +38,24 @@
 <div id="mc_embed_signup">
 
 <form action="https://aaltoes.us2.list-manage.com/subscribe/post?u=550be119ae30d67eafbe4201e&amp;id=f01ee7e18f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
- <h2>Subscribe to our mailing list</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
- <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
+
+    <div id="mc_embed_signup_scroll" class"wrapper">
+
+ <h1>Stay in touch</h1>
+<div class="wrapper-block">
+<div class="form first">
+<a class="fb-button" href="https://www.facebook.com/HELTechMeetup/" title="Facebook"></a>
+<a class="meetup-button" href="https://www.meetup.com/HELTech/" title="Meetup"></a>
+<a class="mail-button" href="mailto:teodora.berg@aaltoes.com" title="Email us"></a>
+</div>
+
+<div class="form second">
+<p>FOMO about what's happening on the Helsinki tech and entrepreneurship scene? Sign up for our newsletter</p>
+
+<!--div class="indicates-required"><span class="asterisk">*</span> indicates required</div-->
+<div class="mc-field-group wrapper-contact-form-email">
+ <!--label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label-->
  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 </div>
  <div id="mce-responses" class="clear">
@@ -51,7 +63,9 @@
   <div class="response" id="mce-success-response" style="display:none"></div>
  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_550be119ae30d67eafbe4201e_f01ee7e18f" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <div class="wrapper-contact-form-submit"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+    </div>
     </div>
 </form>
 </div>
