99abcf75d91c6df5e396034a4c29f3cfb316cf11
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-24 11:16:17 +0200

    added embed mailing list form

--- client/src/landing/BottomRegion.vue
+++ client/src/landing/BottomRegion.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="region region-bottom main">
 <div id="webform-contact">
-<form action="/contact-us" accepted-charset="UTF-8" method="get" id="webform-contact-us" class="webform-processed">
+<form action="https://aaltoes.us2.list-manage.com/subscribe/post?u=550be119ae30d67eafbe4201e&amp;id=f01ee7e18f" accepted-charset="UTF-8" method="post" id="webform-contact-us" class="webform-processed" target="_blank" novalidate>
 <div class="wrapper">
 <h1>Stay in touch</h1>
 <div class="wrapper-block">
@@ -12,13 +12,17 @@
 </div>
 <div class="form second">
 <p>FOMO about what's happening on the Helsinki tech and entrepreneurship scene? Sign up for our newsletter</p>
+
 <div id="wrapper-contact-form-email">
 <input type="email" maxlength="128" name="submitted[email]" id="contact-form-email" size="60" class="email form-text form-email" placeholder="email address" autocomplete="OFF">
 </div>
+
 <div id="wrapper-contact-form-submit">
-<input type="submit" id="contact-form-submit" value="submit" class="form-submit">
+<input type="submit" id="contact-form-submit" value="Subscribe" class="form-submit">
 </div>
 </div>
+
+
 </div>
 </div>
 </form>
