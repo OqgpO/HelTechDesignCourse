e894624ef839244a8fbd383b6b333f070273b43d
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-24 12:25:27 +0200

    webform

--- client/src/landing/BottomRegion.vue
+++ client/src/landing/BottomRegion.vue
@@ -29,11 +29,10 @@
 
 
 <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<!--link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"-->
 <style type="text/css">
  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
- /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-    We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+
 </style>
 <div id="mc_embed_signup">
 
@@ -54,9 +53,9 @@
 
 <!--div class="indicates-required"><span class="asterisk">*</span> indicates required</div-->
 <div class="wrapper-contact-form-email">
- <!--label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label-->
- <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" class="email form-text form-email" placeholder="email address">
+
+  <input type="email" value="" name="EMAIL" maxlength="128" size="60" class="required email" id="mce-EMAIL" class="email form-text form-email" placeholder="email address" autocomplete="OFF">
+
 </div>
  <div id="mce-responses" class="clear">
   <div class="response" id="mce-error-response" style="display:none"></div>
