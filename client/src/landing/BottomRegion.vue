31de626a03de1e41e3b1a6079b966805b125e525
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-24 14:48:17 +0200

    fixing webform

--- client/src/landing/BottomRegion.vue
+++ client/src/landing/BottomRegion.vue
@@ -1,35 +1,9 @@
 <template>
 <div class="region region-bottom main">
 <div id="webform-contact">
-<!-- form action="https://aaltoes.us2.list-manage.com/subscribe/post?u=550be119ae30d67eafbe4201e&amp;id=f01ee7e18f" accepted-charset="UTF-8" method="post" id="webform-contact-us" class="webform-processed" target="_blank" novalidate>
-<div class="wrapper">
-<h1>Stay in touch</h1>
-<div class="wrapper-block">
-<div class="form first">
-<a class="fb-button" href="https://www.facebook.com/HELTechMeetup/" title="Facebook"></a>
-<a class="meetup-button" href="https://www.meetup.com/HELTech/" title="Meetup"></a>
-<a class="mail-button" href="mailto:teodora.berg@aaltoes.com" title="Email us"></a>
-</div>
-<div class="form second">
-<p>FOMO about what's happening on the Helsinki tech and entrepreneurship scene? Sign up for our newsletter</p>
-
-<div id="wrapper-contact-form-email">
-<input type="email" maxlength="128" name="submitted[email]" id="contact-form-email" size="60" class="email form-text form-email" placeholder="email address" autocomplete="OFF">
-</div>
-
-<div id="wrapper-contact-form-submit">
-<input type="submit" id="contact-form-submit" value="Subscribe" class="form-submit">
-</div>
-</div>
-
-
-</div>
-</div>
-</form-->
-
 
 <!-- Begin MailChimp Signup Form -->
-<!--link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"-->
+
 <style type="text/css">
  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 
@@ -51,21 +25,19 @@
 <div class="form second">
 <p>FOMO about what's happening on the Helsinki tech and entrepreneurship scene? Sign up for our newsletter</p>
 
-<!--div class="indicates-required"><span class="asterisk">*</span> indicates required</div-->
- <div class="wrapper-contact-form-email">
-
-  <input type="email" value="" name="EMAIL" maxlength="128" size="60" class="required email" id="mce-EMAIL" class="email form-text form-email" placeholder="email address" autocomplete="OFF">
-    </div>
 
  <div id="mce-responses" class="clear">
   <div class="response" id="mce-error-response" style="display:none"></div>
   <div class="response" id="mce-success-response" style="display:none"></div>
- </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
+ </div>
 
 
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_550be119ae30d67eafbe4201e_f01ee7e18f" tabindex="-1" value=""></div>
 
+ <div class="wrapper-contact-form-email">
+
+  <input type="email" value="" name="EMAIL" maxlength="128" size="60" class="required email" id="mce-EMAIL" class="email form-text form-email" placeholder="email address" autocomplete="OFF">
+    </div>
 
 
     <div class="wrapper-contact-form-submit">
