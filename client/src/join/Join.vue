9686c8d191082873662c5999663da80b609d4c16
Mikko Ylikangas <mikko.ylikangas@aalto.fi>
2017-11-24 13:35:43 +0200

    fsd

--- client/src/join/Join.vue
+++ client/src/join/Join.vue
@@ -20,7 +20,7 @@
 <h1>Want to join?</h1>
 <div id="contribute-join" class="join center-block">
 <h4>You want to pitch, demo, or even launch your product at Hel Tech?</h4>
-<p>Let’s do it! Please fill in the form below. We will get in touch with you asap.<p>
+<p>Let’s do it! Please send us email by clicking on the email link below. We will get in touch with you asap.<p>
 <p>“Big thanks to organizers for the great event, and to everyone attending. It was really a pleasure to see this level event kept in Helsinki, and to see so many people being really interested about the topic. Also thank for letting me share some of my thoughts on the stage!” Bitcoin Entrepreneur, 5.9.2017</p>
 </div>
 
@@ -28,20 +28,20 @@
 </div>
 </div>
 
-<JoinBottomRegion></JoinBottomRegion>
+<BottomRegion></BottomRegion>
 
 </div>
 </template>
 
 
 <script>
-    import JoinBottomRegion from './JoinBottomRegion.vue'
+    import BottomRegion from '../landing/BottomRegion.vue'
     import Navigation from '../common/Navigation.vue'
 
     export default {
         name: 'Join',
         components: {
-            JoinBottomRegion,
+            BottomRegion,
             Navigation,
         }
     }
--- client/src/join/JoinBottomRegion.vue
+++ client/src/join/JoinBottomRegion.vue
@@ -2,7 +2,7 @@
 <div class="region region-bottom main">
 <div id="webform-contact">
 <div class="wrapper">
-<h1>Contact us directly</h1>
+<h1>Stay in touch</h1>
 <div class="wrapper-block">
 <div class="form first">
 <a class="mail-button" href="mailto:teodora.berg@aaltoes.com" title="Email us"></a>
