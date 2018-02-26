99d6fd9a318e1308343b7dbc3c23c44dfab82de1
OqgpO <mikko.ylikangas@aalto.fi>
2018-02-26 20:09:22 +0200

    Update settings.py

--- heltech/settings.py
+++ heltech/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vucg606v(tqfxkh%)diriab*xs)n)adn3wf5hn!f$i!lapyq5m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shapemeal.cs.hut.fi', 'localhost', '127.0.0.1', '130.233.96.67']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'heltech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'heltechdesigncourse',
-        'USER': 'reino',
-        'PASSWORD': 'sindre',
-        'HOST': 'localhost',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': ''
     }
 }
